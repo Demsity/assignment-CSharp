f3dc95452eacb0124ae9395e3acf51f7f4186f8c
Demsity <simonbacklin@hotmail.com>
2023-01-10 10:21:38 +0100

    Finished Setup Folders, Classes and Services.

---
+++ ConsoleApp/Models/Contact.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleApp.Models.Abstracts;
+namespace ConsoleApp.Models;
+
+
+internal interface IContact
+{
+    Guid Id { get; }    
+    string FirstName { get; set; }
+    string LastName { get; set; }
+    string Email { get; set; }
+    string PhoneNumber  { get; set; }
+
+    string DisplayName => $"{FirstName} {LastName}";
+
+}
+internal class Contact : IContact, IAdress
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+    public string StreetName { get; set; } = null!;
+    public string PostalCode { get; set; } = null!;
+    public string City { get; set; } = null!;
+}
---
+++ ConsoleApp/Services/MenuService.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp.Services;
+
+internal class MenuService
+{
+    public static void Run ()
+    {
+        mainMenu();
+
+    }
+
+    private static void mainMenu () {
+        Console.WriteLine("Välkommen till Adressboken");
+        Console.WriteLine("1. Skapa en kontakt");
+        Console.WriteLine("2. Visa alla kontakter");
+        Console.WriteLine("3. Visa en specifik kontakt");
+        Console.WriteLine("4. Ta bort en specifik kontakt");
+        Console.Write("Välj ett av alternativen ovan: ");
+        var options = Console.ReadLine();
+    }
+}
